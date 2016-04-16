5e0fbefbe9b2708a6b5f3644009fefda8a521f55
BEN1JEN <benpheard@gmail.com>
2016-04-15 22:44:23 -0600

    added test file

---
+++ code/Startup.lua
@@ -0,0 +1,10 @@
+local blocks = {}
+blocks["white"] = string.char(32)
+blocks["light_gray"] = string.char(176)
+blocks["gray"] = string.char(177)
+blocks["dark_gray"] = string.char(178)
+blocks["black"] = string.char(219)
+
+print(blocks["gray"])
+while true do
+end
