1a2367a018eb83de64881b2dcb6cf6bc177908cd
BEN1JEN <benpheard@gmail.com>
2016-04-15 23:21:36 -0600

    added the skelaton

--- code/Startup.lua
+++ code/Startup.lua
@@ -4,7 +4,3 @@ blocks["light_gray"] = string.char(176)
 blocks["gray"] = string.char(177)
 blocks["dark_gray"] = string.char(178)
 blocks["black"] = string.char(219)
-
-print(blocks["gray"])
-while true do
-end
---
+++ code/func.lua
@@ -0,0 +1,23 @@
+function draw(Screen)
+
+	
+
+end
+
+function drawTI(ti)
+
+	
+
+end
+
+function userInput()
+
+	
+
+end
+
+function logic(UI)
+
+	
+
+end
