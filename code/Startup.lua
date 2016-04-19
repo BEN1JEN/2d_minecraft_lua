35d424344389c66945efa47052012ec0ddfd258a
BEN1JEN <benpheard@gmail.com>
2016-04-19 13:12:30 -0600

    startup function
    added a starup function and a test to go with it.

--- code/Startup.lua
+++ code/Startup.lua
@@ -1,6 +1,26 @@
-local blocks = {}
-blocks["white"] = string.char(32)
-blocks["light_gray"] = string.char(176)
-blocks["gray"] = string.char(177)
-blocks["dark_gray"] = string.char(178)
-blocks["black"] = string.char(219)
+function startUp(blocks, screen)
+
+	blocks["white"] = string.char(32)
+	blocks["light_gray"] = string.char(176)
+	blocks["gray"] = string.char(177)
+	blocks["dark_gray"] = string.char(178)
+	blocks["black"] = string.char(219)
+
+	for x = 1, 80 do
+
+		screen[x] = {}
+
+	end
+
+	for y = 1, 45 do
+
+		for x = 1, 80 do
+	
+			screen[x][y] = "white"
+	
+		end
+
+	end
+
+end
+
--- code/main.lua
+++ code/main.lua
@@ -1,1 +1,16 @@
-dofile("func.lua")
+require("Startup")
+require("func")
+
+local blocks = {}
+local screen = {}
+
+startUp(blocks, screen)
+
+
+while true do
+
+	print(screen[1][2])
+
+end
+
+
